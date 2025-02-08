9e1d761efb329fb871e4e58d8a277996e3ef117a
Derrick Ma <derrickma2005@gmail.com>
2025-02-08 17:00:56 -0500

    Created app

---
+++ phishgame/app/index.tsx
@@ -0,0 +1,125 @@
+import { TagsList } from '@/components/TagsList';
+import { ThemedText } from '@/components/ThemedText';
+import { ThemedView } from '@/components/ThemedView';
+import { Text, View, StyleSheet, Image, TextInput, TouchableOpacity} from 'react-native';
+import {SafeAreaView, SafeAreaProvider} from 'react-native-safe-area-context';
+import React from 'react';
+import { useState } from 'react';
+
+export default function HomeScreen() {
+  const [difficulty, setDifficulty] = useState(0);
+  const [emailCount, setEmailCount] = useState(10); 
+  return (
+    <SafeAreaProvider>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.header}>
+            <Text style={styles.title}>Phishgame</Text>
+            <View style={styles.searchBar}> 
+              <Image
+              source={require('@/assets/images/search-icon.png')}
+              style={styles.searchIcon}
+            />
+            <TextInput
+              style={styles.search}
+              placeholder="Search">
+                </TextInput>
+            </View>
+            
+        </View>
+        <View style={styles.body}>
+          <View style={styles.sidebar}>
+          <Image
+              source={require('@/assets/images/search-icon.png')}
+              style={styles.searchIcon}
+            />
+          </View>
+          <View style={styles.main}>
+            <TagsList difficulty = {difficulty} selectIndex = {setDifficulty}/>
+            <View style={styles.emails}>
+
+            </View>
+          </View>
+        </View>
+        </SafeAreaView>
+    </SafeAreaProvider>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  header: {
+    flex: 0.05,
+    color: 'black',
+    backgroundColor: 'white',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    padding: 15,
+    borderColor: 'light-grey',
+    borderBottomWidth: 1,
+    flexDirection: 'row',
+  },
+  title: {
+    flex: 0.3,
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  searchBar: {
+    backgroundColor: '#D3D3D3',
+    flex: 0.4,
+    flexDirection: 'row',
+    borderColor: 'black',
+    borderWidth: 1,
+    borderRadius: 20,
+    height: 40,
+  },
+  searchIcon: {
+    width: 15,
+    height: 15,
+    marginLeft: 5,
+    marginTop: 12,
+    marginRight: 5,
+  },
+  search: {
+    width: 450,
+    fontSize: 15,
+  },
+  body: {
+    flex: 0.95,
+    justifyContent: 'flex-start',
+    flexDirection: 'row',
+  },
+  sidebar: {
+    flex: 0.05,
+    backgroundColor: '#D3D3D3',
+    borderColor: 'black',
+    borderRightWidth: 1,
+    flexDirection: 'column',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    textAlign: 'center',
+  },
+  main: {
+    flex: 0.95,
+    flexDirection: 'column',
+  },
+  emails: {
+    flex: 1,
+    flexDirection: 'column',
+  },
+  emailBox: {
+    flex: 0.1,
+    borderBottomWidth: 1,
+    borderColor: 'black',
+    justifyContent: 'flex-start',
+  },
+  selectedEmailBox: {
+    flex: 0.1,
+    borderBottomWidth: 1,
+    borderColor: 'black',
+    backgroundColor: 'light-grey',
+    textDecorationLine: 'underline',
+    justifyContent: 'flex-start',
+  }
+});
---
+++ phishgame/components/TagsList.tsx
@@ -0,0 +1,84 @@
+import { Text, View, StyleSheet, Image,  TouchableOpacity} from 'react-native';
+import React from 'react';
+
+interface TagsProps {
+  difficulty: number;
+  isSelected: boolean;
+  onPress: () => void;
+}
+const Tag = ({difficulty, isSelected, onPress} : TagsProps) => {
+  const imageUrls = [require('@/assets/images/PhishNoob.png'), 
+    require('@/assets/images/Phishmediate.png'), 
+    require('@/assets/images/PhishMaster.png'),
+  require('@/assets/images/PhishNoobSelect.png'),
+  require('@/assets/images/PhishmediateSelect.png'),
+  require('@/assets/images/PhishMasterSelect.png')];
+
+  return (
+    <TouchableOpacity onPress={onPress} style={isSelected ? styles.selectedTagBox : styles.tagBox}>
+      <Image 
+      source = {imageUrls[difficulty + (isSelected ? 3 : 0)]}
+      style={styles.Icon}
+      />
+      <Text>{difficulty === 0 ? 'Phish Noob' : difficulty=== 1 ? 'Phismediate' : 'Phish Master'} </Text>
+    </TouchableOpacity>
+  );
+}
+interface TagListProps {
+  difficulty: number;
+  selectIndex: (input : number) => void;
+}
+const TagList = ({difficulty, selectIndex} : TagListProps) => {
+  return (
+    <View style={styles.tags}>
+      {[...Array(3)].map((_, index) => (
+        <Tag
+          key={index}
+          difficulty={index}
+          isSelected={index === difficulty} 
+          onPress={() => selectIndex(index)} 
+        />
+      ))}
+    </View>
+  );
+}
+
+export function TagsList({difficulty, selectIndex} : TagListProps) {
+    return (
+        <TagList difficulty= {difficulty} selectIndex={selectIndex}/>
+    );
+}
+
+const styles = StyleSheet.create({
+    tags: {
+        flex: 0.1,
+        flexDirection: 'row',
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+        textAlign: 'center',
+        borderBottomWidth: 1,
+        borderColor: 'black',
+      },
+      tagBox: {
+        flex: 1/3,
+        padding: 10,
+        borderColor: 'black',
+        borderRightWidth: 1,
+        flexDirection: 'row',
+      },
+      selectedTagBox: {
+        flex: 1/3,
+        padding: 10,
+        borderColor: 'black',
+        borderRightWidth: 1,
+        backgroundColor: 'light-grey',
+        textDecorationLine: 'underline',
+        flexDirection: 'row',
+      },
+      Icon: {
+        width: 25,
+        height: 25,
+        marginLeft: 5,
+        marginRight: 10,
+      },
+    });
